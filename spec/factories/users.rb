3025aed6eb045b4a34362f6ca5d8543df82038db
Jack-Barry <jackaldenbarry@gmail.com>
2017-12-21 22:15:24 -0600

    Authenticate Users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    username        { Faker::Internet.user_name }
-    password_digest { Faker::Internet.password } 
+    username { Faker::Internet.user_name }
+    password { Faker::Internet.password }
   end
 end
